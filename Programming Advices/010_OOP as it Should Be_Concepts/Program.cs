0fffa24723bc368b021e253102dfd762e3eabf32
Saleem Kassab <79147255+SaleemAdhamKassab@users.noreply.github.com>
2023-01-19 23:37:48 +0300

    Add files via upload

---
+++ Programming Advices/010_OOP as it Should Be_Concepts/Program.cs
@@ -0,0 +1,21 @@
+﻿using Lab;
+
+namespace lab
+{
+    class Program
+    {
+        static void Main()
+        {
+            Person person1 = new Person(10, "Saleem", "Kassab", "e.s.a.kassab@gmail.com",
+                                        "00963-959-270-763");
+            person1.printInfo();
+
+            Console.WriteLine();
+
+            Employee employee = new Employee(20, "Nassem", "Kassab", "nassem.kassab@gmail.com",
+                                            "00963-565-364-643",
+                                            2000, "FMU", "Electronic Engineer");
+            employee.printInfo();
+        }
+    }
+}
