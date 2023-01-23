7ec03369880dacb91cd3d5c89a65ed9e083f0715
Saleem Kassab <79147255+SaleemAdhamKassab@users.noreply.github.com>
2023-01-23 23:22:39 +0300

    Add files via upload

---
+++ Programming Advices/010_OOP as it Should Be_Concepts/044_String Library Project_Solution/Program.cs
@@ -0,0 +1,48 @@
+﻿using Lab;
+using Lab.Classes;
+
+namespace lab
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            SaleemString obj = new SaleemString("Saleem Kassab");
+
+
+            // 1
+            obj.length();
+            SaleemString.length("saleem kassab");
+
+            //2
+            obj.countWords();
+            SaleemString.countWords("saleem kassab");
+
+            //3
+            obj.capitalizeFirstLetterOfEachWord();
+            SaleemString.capitalizeFirstLetterOfEachWord("saleem kassab");
+
+            //4
+            obj.lowerFirstLetterOfEachWord();
+            SaleemString.lowerFirstLetterOfEachWord("SALEEM KASSAB");
+
+
+            //5
+            obj.ToUpper();
+            SaleemString.ToUpper("saleem kassab");
+
+            //6
+            obj.ToLower();
+            SaleemString.ToLower("SALEEM KASSAB");
+
+            //7
+            obj.Invert();
+            SaleemString.Invert("Saleem");
+
+
+
+            Console.WriteLine(obj.Invert());
+        }
+    }
+}
