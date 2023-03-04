5020f64bd7f3cdf7ab31f08549a3bd4f4e745414
Saleem Kassab <79147255+SaleemAdhamKassab@users.noreply.github.com>
2023-03-04 22:54:06 +0300

    Add files via upload

---
+++ Programming Advices/011_OOP as it Should Be_Applications/015_Lesson10_Delete Client Screen/SaleemLibraries/SaleemPeriod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Classes
+{
+    internal class SaleemPeriod
+    {
+        public clsDate StartDate;
+        public clsDate EndDate;
+
+        SaleemPeriod(clsDate StartDate, clsDate EndDate)
+        {
+            this.StartDate = StartDate;
+            this.EndDate = EndDate;
+
+        }
+        static bool IsOverlapPeriods(SaleemPeriod Period1, SaleemPeriod Period2)
+        {
+
+            if (
+                SaleemDate.CompareDates(Period2.EndDate, Period1.StartDate) == SaleemDate.enDateCompare.Before
+                ||
+                SaleemDate.CompareDates(Period2.StartDate, Period1.EndDate) == SaleemDate.enDateCompare.After
+                )
+                return false;
+            else
+                return true;
+
+        }
+    }
+}
