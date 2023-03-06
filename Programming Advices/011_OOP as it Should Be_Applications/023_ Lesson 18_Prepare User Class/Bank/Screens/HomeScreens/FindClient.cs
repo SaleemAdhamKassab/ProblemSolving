f4fc65d1d48a9913eca59d1e69d5eca698e6714c
Saleem Kassab <79147255+SaleemAdhamKassab@users.noreply.github.com>
2023-03-06 14:58:51 +0300

    Add files via upload

---
+++ Programming Advices/011_OOP as it Should Be_Applications/023_ Lesson 18_Prepare User Class/Bank/Screens/HomeScreens/FindClient.cs
@@ -0,0 +1,28 @@
+﻿using Lab.Class.Bank;
+using Labs.Bank.Screens;
+using Labs.Bank.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Bank.Screens
+{
+    internal class FindClient : Screen
+    {
+        public static void Find()
+        {
+            string accountNumber = "";
+
+            drawScreenHeader("Find Client");
+            accountNumber = readClientOneInfo("Enter Account Number: ").ToString();
+
+            while (!IsClientExist(accountNumber))
+                accountNumber = readClientOneInfo("Account Number (" + accountNumber + ") is not found, choose another one: ").ToString();
+
+            PrintClient(findClient(accountNumber));
+        }
+
+    }
+}
---
+++ Programming Advices/011_OOP as it Should Be_Applications/023_ Lesson 18_Prepare User Class/Bank/Shared/SaleemLibraries/SaleemInputValidate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Classes
+{
+    internal class SaleemInputValidate
+    {
+        public static bool IsNumberBetween(int number, int from, int to) => number >= from && number <= to;
+        public static bool IsNumberBetween(double number, double from, double to) => number >= from && number <= to;
+        public static bool IsDouble(string valueToTest)
+        {
+            if (double.TryParse(valueToTest, out double d) && !Double.IsNaN(d) && !Double.IsInfinity(d))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public static double readNumber()
+        {
+            string inputNumber = Console.ReadLine();
+
+            while (!IsDouble(inputNumber))
+            {
+                Console.Write("Invalid Number, Enter a gain:");
+                inputNumber = Console.ReadLine();
+            }
+
+            return double.Parse(inputNumber);
+        }
+
+        public static double readNumberBetween(double number, double from, double to)
+        {
+            while (!IsNumberBetween(number,from,to))
+            {
+                number = double.Parse(Console.ReadLine());
+                Console.WriteLine("Number is not within range {0} and {1}: ", from, to);
+            }
+
+            return number;
+        }
+
+    }
+}
