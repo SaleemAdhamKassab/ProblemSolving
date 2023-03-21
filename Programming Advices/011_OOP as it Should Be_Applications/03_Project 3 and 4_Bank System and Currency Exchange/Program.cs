6de9f9b587577ea229ae9c27bb1518103b92acf7
Saleem Kassab <79147255+SaleemAdhamKassab@users.noreply.github.com>
2023-03-21 15:19:08 +0300

    Create Program.cs

---
+++ Programming Advices/011_OOP as it Should Be_Applications/03_Project 3 and 4_Bank System and Currency Exchange/Program.cs
@@ -0,0 +1,13 @@
+using Lab.Bank.Views.ManageUsers;
+
+namespace lab
+{
+    class Program
+    {
+        static void Main()
+        {
+            Home home = new Home();
+            home.perfoemUserLoginView();
+        }
+    }
+}
