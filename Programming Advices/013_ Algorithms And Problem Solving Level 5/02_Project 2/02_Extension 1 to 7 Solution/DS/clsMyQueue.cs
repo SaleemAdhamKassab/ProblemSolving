244ab5175332950009534a21ce2784437ee71991
Saleem Kassab <79147255+SaleemAdhamKassab@users.noreply.github.com>
2023-04-05 12:48:39 +0300

    Create clsMyQueue.cs

---
+++ Programming Advices/013_ Algorithms And Problem Solving Level 5/02_Project 2/02_Extension 1 to 7 Solution/DS/clsMyQueue.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.DS
+{
+    internal class clsMyQueue<T>
+    {
+        clsDblLinkedList<T> myList = new clsDblLinkedList<T>();
+
+        public void push(T data) => myList.insertAtEnd(data);
+        public void pop() => myList.deleteFirstNode();
+        public void print() => myList.printList();
+        public int size() => myList.size();
+        public bool isEmpty() => myList.isEmpty();
+        public T front() => myList.getNodeByIndex(0).Data;
+        public T back() => myList.getNodeByIndex(size() - 1).Data;
+        public T getNodeByIndex(int nodeIndex) => myList.getNodeByIndex(nodeIndex).Data;
+        public void reverse() => myList.reverse();
+        public void updateNodeByIndex(int nodeIndex, T data) => myList.UpdateNodeByIndex(nodeIndex, data);
+        public void insertAfterNodeIndex(int nodeIndex, T data) => myList.insertAfter(nodeIndex, data);
+        public void insertAtFront(T data) => myList.insertAtBeging(data);
+        public void insertAtBack(T data) => myList.insertAtEnd(data);
+        public void clear() => myList.clear();
+    }
+}
