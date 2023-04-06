ace2b1324ea9e85b52effce45d5403f6529bee8c
Saleem Kassab <79147255+SaleemAdhamKassab@users.noreply.github.com>
2023-04-06 14:52:03 +0300

    Create MyQueueArr.cs

---
+++ Programming Advices/013_ Algorithms And Problem Solving Level 5/05_Project 5_Queue Based On Dynamic Array/DS/MyQueueArr.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.DS
+{
+    internal class MyQueueArr<T>
+    {
+        MyDynamicArray<T> MyDynamicArray = new MyDynamicArray<T>(0);
+
+        public void push(T data) => MyDynamicArray.insertElementAtEnd(data);
+        public void pop() => MyDynamicArray.deleteFirstItem();
+        public void print() => MyDynamicArray.printList();
+        public int size() => MyDynamicArray.size();
+        public T front() => MyDynamicArray.getItemByIndex(0);
+        public T back() => MyDynamicArray.getItemByIndex(size() - 1);
+        public T getNodeByIndex(int index) => MyDynamicArray.getItemByIndex(index);
+        public void reverse() => MyDynamicArray.reverse();
+        public void updateNodeByIndex(int index, T data) => MyDynamicArray.setItem(index, data);
+        public void insertAfterNodeIndex(int index, T data) => MyDynamicArray.insertAfterIndex(index, data);
+        public void insertAtFront(T data) => MyDynamicArray.insertElementAtBegining(data);
+        public void insertAtBack(T data) => MyDynamicArray.insertElementAtEnd(data);
+        public void clear() => MyDynamicArray.clear();
+    }
+}
